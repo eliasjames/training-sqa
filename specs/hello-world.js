ccdf9aaf95beba614f35cf774eb113c7b927efcb
Elias Carlston <elias@eliasjames.com>
2016-09-09 12:38:33 -0500

    Remove mocks left over from setup

--- specs/hello-world.js
+++
@@ -1,9 +0,0 @@
-describe('hello-world', function () {
-  it('should return a string', function () {
-    var helloWorldFactory = require('../src/hello-world.js');
-    var helloWorldInstance = helloWorldFactory();
-    var testString = helloWorldInstance();
-
-    expect(testString).toEqual('hello world');
-  });
-});
