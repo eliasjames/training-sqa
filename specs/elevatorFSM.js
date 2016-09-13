9a26a913f94ad2b0c8e483f8292df96b4b22b859
Elias Carlston <elias@eliasjames.com>
2016-09-13 16:37:17 -0500

    format/style

--- specs/elevatorFSM.js
+++ specs/elevatorFSM.js
@@ -1,17 +1,24 @@
-describe('elevatorFSM', function () {
-  var elevatorFactory = require('../src/elevatorFSM.js');
-  var elevatorFSM = elevatorFactory();
+describe( 'elevatorFSM', function()  {
+  var elevatorFactory = require( '../src/elevatorFSM.js' );
+  var elevatorFSM = elevatorFactory(1);
 
-  it('should start out waiting', function () {
+  it( 'should start out waiting', function()  {
     var testString = elevatorFSM.getMachineState();
 
-    expect(testString).toEqual('WAITING');
+    expect( testString ).toEqual( 'WAITING' );
   });
 
-  it('should transition to another state', function () {
+  it( 'should transition to another state', function()  {
     elevatorFSM.move(); 
     var testString = elevatorFSM.getMachineState();
 
-    expect(testString).toEqual('MOVE_ONE_FLOOR');
+    expect( testString ).toEqual( 'MOVE_ONE_FLOOR' );
+  });
+
+  it('should return the initial floor number', function()  {
+    var floorNumber = elevatorFSM.getFloorNumber();
+    console.log( floorNumber );
+    // depends on the config value on line 3
+    expect( floorNumber ).toEqual(1);
   });
 });
