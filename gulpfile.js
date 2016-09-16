8023fb21f6239ade942c6823afa1d81a059d7648
Elias Carlston <elias@eliasjames.com>
2016-09-16 12:00:01 -0500

    Improve jshint, watch tools

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,25 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var karmaServer = require('karma').Server;
+var watch = require('gulp-watch');
+var SRC_FILES = 'src/**/*.js';
+var SPEC_FILES = 'specs/**/*.js';
 
-gulp.task('default', () =>
-    gulp.src(['src/**/*.js'])
-        .pipe(jshint('.jshintrc'))
-        .pipe(jshint.reporter('jshint-stylish'))
-);
+gulp.task( 'default', ['hint', 'test']);
+gulp.task( 'hint', function() {
+  gulp.src( [SRC_FILES, SPEC_FILES] )
+    .pipe( jshint( '.jshintrc' ))
+    .pipe( jshint.reporter( 'jshint-stylish' ))
+});
+gulp.task('watch-hint', function () {
+  return watch([SRC_FILES, SPEC_FILES], { ignoreInitial: true })
+    .pipe( jshint( '.jshintrc' ))
+    .pipe( jshint.reporter( 'jshint-stylish' ))
+});
+gulp.task( 'test', function( done ) {
+  var karmaConfig = {
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  };
+  new karmaServer( karmaConfig, done ).start();
+});
